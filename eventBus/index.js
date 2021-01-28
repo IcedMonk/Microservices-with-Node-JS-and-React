7dc9d8130012f50135812cc9d7a2056b31c276a4
Samyek S <sonawanesamyek@gmail.com>
2021-01-28 10:18:44 +0000

    Event sync done, if query goes down it takes back the data on startup

--- eventBus/index.js
+++ eventBus/index.js
@@ -5,15 +5,31 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
 
-    axios.post('https://4000-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event);
-    axios.post('https://4003-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event);
-    axios.post('https://4001-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event);
-    axios.post('https://4002-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event);
+    events.push(event);
+
+    axios.post('https://4000-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('https://4003-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('https://4001-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event).catch((err) => {
+        console.log(err.message);
+    });
+    axios.post('https://4002-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events', event).catch((err) => {
+        console.log(err.message);
+    });
 
     res.send({ status: 'OK' })
+})
+
+app.get('/events', (req, res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,12 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
@@ -35,15 +31,29 @@ app.post('/events', (req, res) => {
             return comment.id === id;
         });
 
-
         comment.status = status;
         comment.content = content;
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('server is listening on 4002');
+    const res = await axios.get('https://4005-daac66df-85fc-40b8-8dd2-fd5026da8f90.ws-us03.gitpod.io/events');
+    for (let event of res.data) {
+        console.log('Processing Event: ', event.type);
+        handleEvent(event.type, event.data);
+    }
 })
