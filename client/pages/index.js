8e50be7d2ab5d9fb3a5d6bf360305be8e0a491f1
Samyek S <39209724+IcedMonk@users.noreply.github.com>
2021-03-06 08:59:01 +0000

    added client with docker/kube configs

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div></div>
+      <Head>
+        <title>Create Next App</title>
+        
+      </Head>
+
+      <h1>hi there</h1>
+       
+    </div>
+  )
+}
